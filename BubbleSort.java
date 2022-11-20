206d0305555bd2c119df662a947583b9c0c13a15
TMina000 <74081618+TMina000@users.noreply.github.com>
2022-11-20 16:59:21 +0900

    インデント修正

--- BubbleSort.java
+++ BubbleSort.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
  */
 class BabbitSort {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
         try (Scanner scan = new Scanner(System.in)) {
             System.out.println("ソートしたい数字を入力してください。(入力終了はEnter)");
             System.out.println("例) > 1 2 10 3 39");
@@ -16,18 +16,18 @@ class BabbitSort {
 
             // 入力した数字を配列にする
             int[] array = makeArrayInt(scan);
-            
+
             // 数字の配列をバブルソートする
             array = bubbleSort(array);
 
-            for(int num: array){
+            for (int num : array) {
                 System.out.print(num + " ");
             }
 
         } catch (NumberFormatException e) {
             e.printStackTrace();
         }
-	}
+    }
 
     /**
      * 数字の配列を作成する関数
@@ -35,16 +35,16 @@ class BabbitSort {
      * @param scan ソート対象の数字の標準入力
      * @return 数字の配列
      */
-    private static int[] makeArrayInt(Scanner scan){
+    private static int[] makeArrayInt(Scanner scan) {
         String str = scan.nextLine();
 
         String[] array = str.split(" ");
         int[] arrayInt = new int[array.length];
 
-        for(int i = 0; i < array.length; i++) {
+        for (int i = 0; i < array.length; i++) {
             arrayInt[i] = Integer.valueOf(array[i]);
         }
-      
+
         return arrayInt;
     }
 
@@ -54,24 +54,24 @@ class BabbitSort {
      * @param array ソートしたい配列
      * @return ソート結果
      */
-    private static int[] bubbleSort(int[] array){
-        
-        for(int i = 0; i < array.length - i; i++){
-            for(int j = 0; j < array.length - 1; j++){
-                
+    private static int[] bubbleSort(int[] array) {
+
+        for (int i = 0; i < array.length - i; i++) {
+            for (int j = 0; j < array.length - 1; j++) {
+
                 int left = array[j];
                 int right = array[j + 1];
                 int backup = 0;
 
                 // 左、左+1(右)の値が左>右の場合、配列を入れ替える
-                if(left > right){
+                if (left > right) {
                     backup = right;
                     array[j + 1] = left;
                     array[j] = backup;
                 }
             }
         }
-        
+
         return array;
     }
 }
